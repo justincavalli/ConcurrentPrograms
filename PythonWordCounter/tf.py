722a9d7db8d7efb61da9dcb03220220d992896fc
Justin <jcavalli@sandiego.edu>
2022-10-21 17:05:36 -0700

    implemented pyhon threading

--- PythonWordCounter/tf.py
+++ PythonWordCounter/tf.py
@@ -1,5 +1,7 @@
 import re, sys, collections
 import os
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
 # stopwords = set(open('stop_words').read().split(','))
 # words = re.findall('\w{3,}', open(sys.argv[1]).read().lower())
@@ -8,14 +10,30 @@ import os
 #     print(w, '-', c)
 
 totalCount = collections.Counter()
+lock = threading.Lock()        
 
+def calcWords(file):
+    stopwords = set(open('stop_words').read().split(','))
+    words = re.findall('\w{3,}', open(file).read().lower())
+    counts = collections.Counter(w for w in words if w not in stopwords)
+    
+    # guard shared mutable state totalCount with a lock
+    lock.acquire()
+    totalCount.update(counts)
+    lock.release()
+
+
+fileList = []
+
+# create a list of all the txt files to be read
 for files in os.listdir("./"):
     if files.endswith(".txt"):
-        stopwords = set(open('stop_words').read().split(','))
-        words = re.findall('\w{3,}', open(files).read().lower())
-        counts = collections.Counter(w for w in words if w not in stopwords)
-        totalCount.update(counts)
+        fileList.append(files)
+
+# start a thread for each txt file
+with ThreadPoolExecutor() as exec:
+    for file in fileList:
+        exec.submit(calcWords, file)
+
 for(w, c) in totalCount.most_common(25):
     print(w, '-', c)
-        
-        
