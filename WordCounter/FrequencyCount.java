6f1084d7078e1ff07890aab4bc66cd6ccefc8714
Justin <jcavalli@sandiego.edu>
2022-10-24 12:05:07 -0700

    changed timing

--- WordCounter/FrequencyCount.java
+++ WordCounter/FrequencyCount.java
@@ -15,7 +15,6 @@ import java.util.Set;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -139,15 +138,16 @@ public class FrequencyCount {
 	    e.printStackTrace();
 	}
 
+	long end = System.nanoTime();
+
+	long elapsed = end - start;
+
 	try {
 		//block until exec has terminated so the final code doesn't run prematurely
 		boolean finished = exec.awaitTermination(1, TimeUnit.MINUTES);
 	} catch(InterruptedException e) {
 		System.out.println(e);
 	}
-	long end = System.nanoTime();
-
-	long elapsed = end - start;
 
 	System.out.println(c);
 	System.out.println("Elapsed time: " + elapsed/1000000 + "ms");
